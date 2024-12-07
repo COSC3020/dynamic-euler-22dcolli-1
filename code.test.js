0409c35a8c90bc31b01d3086b755ad9bbe149fa3
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-12-07 00:27:39 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -6,13 +6,13 @@ eval(fs.readFileSync('code.js')+'');
 //assert(e(10) === 2.7182818011463845);
 
 //from last semesters repo
-assert(e(10) === 2.7182818011463845);
 assert(e(5) >= 2.716 - .1 && e(5) <= 2.716 + .1);
 assert(e(100) >= 2.6881 - .1 && e(100) <= 2.6881 + .1);
 
 //added on for new semester 
+assert(e(10) > 2.71828 - 0.00001 && e(10) < 2.71828 + 0.00001);
 //definative values
-assert.strictEqual(e(0) === 1);
-assert.strictEqual(e(1) === 2);
+assert(e(0) === 1);
+assert(e(1) === 2);
 //negative check
-assert.strictEqual(e(-1) === 1);
+assert(e(-1) === 1);
