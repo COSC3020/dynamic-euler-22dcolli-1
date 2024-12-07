79596552bbbce60b6c49ce02b9e2e4b6a88bae08
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-12-07 00:02:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+/*
 function factorial(n) {
     if(n === 0) return 1;
     else return n * factorial(n - 1);
@@ -7,3 +8,21 @@ function e(n) {
     if(n === 0) return 1;
     else return 1.0 / factorial(n) + e(n - 1);
 }
+*/
+
+function dynaE(n){
+
+    let fact = 1;
+    let e = 1;
+
+    for(let i=1; i <= n; i++){
+
+        fact = (fact * i);
+        e = (e+(1 / fact));
+    }
+    return e;
+
+}
+
+//console.log(dynaE(2))
+//console.log(dynaE(3))
